935d47f5814229e01fb7319cc508a232e83e02e5
dagab8 <151142225+dagab8@users.noreply.github.com>
2024-02-16 00:19:23 +0100

    Create pk1.js
    krotoszynska.pl

---
+++ pk1.js
@@ -0,0 +1,69 @@
+<script async src='https://securepubads.g.doubleclick.net/tag/js/gpt.js'></script>
+<script>
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+   googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/belka_sticky', [[320,50],[320,100],[360,50],[360,100],[750,100],[970,90]], 'div-gpt-ad-2356994-1')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/inphoto_1', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-2')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/inphoto_2', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-3')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/inphoto_3', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-4')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/inphoto_4', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-5')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_1', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250]], 'div-gpt-ad-2356994-6')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_2', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250]], 'div-gpt-ad-2356994-7')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_3', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250]], 'div-gpt-ad-2356994-8')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_4', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250]], 'div-gpt-ad-2356994-9')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_art_1', [[300,250],[300,600],[336,280],[420,500],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-10')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_art_2', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-11')
+             .addService(googletag.pubads());
+
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_half', [[120,600],[160,600],[300,250],[300,600],[325,600],[336,280]], 'div-gpt-ad-2356994-13')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_half_1', [[120,600],[160,600],[300,250],[300,600],[325,600],[336,280]], 'div-gpt-ad-2356994-14')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/lazy_1', [[300,250],[336,280],[420,500],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-15')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/lazy_2', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-16')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/lazy_3', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-17')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/lazy_4', [[300,250],[336,280],[750,100],[750,200],[750,300]], 'div-gpt-ad-2356994-18')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_middle_1', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250],[1200,300]], 'div-gpt-ad-2356994-19')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_middle_2', [[300,250],[336,280],[750,100],[750,200],[750,300],[800,400],[970,90],[970,250],[1200,300]], 'div-gpt-ad-2356994-20')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_middle_3', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250],[1200,300]], 'div-gpt-ad-2356994-21')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_middle_4', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250],[1200,300]], 'div-gpt-ad-2356994-22')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_middle_5', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250],[1200,300]], 'div-gpt-ad-2356994-23')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_rec', [[300,250],[336,280]], 'div-gpt-ad-2356994-24')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_rec_1', [[300,250],[336,280]], 'div-gpt-ad-2356994-25')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_rec_2', [[300,250],[336,280]], 'div-gpt-ad-2356994-26')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_rec_3', [[300,250],[336,280]], 'div-gpt-ad-2356994-27')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_sky', [[120,600],[160,600]], 'div-gpt-ad-2356994-28')
+             .addService(googletag.pubads());
+    googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_top', [[300,250],[336,280],[750,100],[750,200],[750,300],[970,90],[970,250],[1200,300],[1200,600]], 'div-gpt-ad-2356994-29')
+             .addService(googletag.pubads());
+googletag.defineSlot('/22860782424/WTG_container/krotoszynska.pl/slot_bottom', [[750,200],[750,100],[970,250],[336,280],[300,250],[970,90],[750,300],[1200,300]], 'div-gpt-ad-2356994-12')
+             .addService(googletag.pubads());
+
+    googletag.pubads().setForceSafeFrame(false);
+    googletag.pubads().setCentering(true);
+    googletag.enableServices();
+  });
+</script>
